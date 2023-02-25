106b1299575e3c83a316ad48ab317923ee74204c
purpleSage <hectormza21@gmail.com>
2023-02-25 17:26:01 -0400

    add back to top button, smooth scrolling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ import { BiMenu } from 'react-icons/bi';
 
 const Header = () => {
 	return (
-		<header className="top-0 z-20 flex w-full items-center justify-between bg-slate-500/20 p-3 backdrop-blur">
+		<header
+			id="header"
+			className="top-0 z-20 flex w-full items-center justify-between bg-slate-500/20 p-3 backdrop-blur"
+		>
 			<h3 className="ml-3 flex items-center gap-1 font-display text-lg tracking-wide text-white">
 				<span className="fill-orange-500">
 					<DumbellSVG />
